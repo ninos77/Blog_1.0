c485bd67dc9ebed8d11b45e8d35b6561b358d8cb
ninos77 <ninos_zaiya@hotmail.com>
2020-11-06 13:54:08 +0100

    Added models for post

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class AdminPost(admin.ModelAdmin):
+    list_filter = ['publishing_date']
+    list_display = ['title', 'user', 'publishing_date']
+    search_fields = ['title', 'content']
+
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, AdminPost)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    publishing_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(blank=True, null=True, upload_to='uploads/')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
